d22af10eec5764d4cb50489b0a1e80a034313cf5
Martin PAUCOT <m.paucot@kmp.agency>
2022-05-29 16:42:04 +0200

    vault

--- config.hcl
+++ config.hcl
@@ -1,6 +1,11 @@
 disable_mlock = true
 ui = true
 
+listener "tcp" {
+    address = "0.0.0.0:8200"
+    tls_disable = "true"
+}
+
 storage "postgresql" {
     connection_url = "${DATABASE_URL}"
 }
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,4 @@ export DATABASE_URL=$(/get-qovery-env.sh POSTGRESQL ${PGSQL_ID} DATABASE_URL_INT
 defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
 envsubst "$defined_envs" < "/vault/config/config.hcl.template" > "/vault/config/config.hcl"
 
-docker-entrypoint.sh server -config /vault/config/config.hcl
+docker-entrypoint.sh vault server -config /vault/config/config.hcl
